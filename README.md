5f5d83e7f2e7c9ba3a2b3643688bc2982d208a1f
chase <chase.entwistle@gmail.com>
2022-11-10 16:29:40 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# dap-hot-config.nvim
+
+Adds commands to NeoVim that let you configure (nvim-dap)[https://github.com/mfussenegger/nvim-dap] without adding launch.json files to your project or editing your global DAP configurations.
+
+TODO: Make proper documentation instead of putting all this in README.md
+
+## Commands
+
+### :Argv
+
+List or modify the arguments that will be passed to your program when debugging. When successful, prints out the table of arguments. The Vim command can look like any of the following:
+
+Argv .................. only prints the args
+Argv pop .............. removes the last item in args
+Argv clear ............ sets args to an empty table
+Argv <index> .......... sets args[index] to an empty string
+Argv <index> <value> .. sets args[index] to value. Value can start with whitespace, just put the whitespace after the separating space between <index> and <value>.
+
+For those last two uses, the index supplied cannot be greater than any index that was not yet supplied.
+
+### :Main
+
+I often want to debug a module or a library that doesn't do anything on its own. To do that, you need a separate program that runs tests on the module so that you can debug the module by starting a debug session on the tester program. This command is a solution to keep you from having to flip back and forth between the main file that you want to run, and the module that has all the bugs.
+
+This command modifies the "program" entry of your configuration. Did you know that that entry can be either a string or a function that returns a string? If you want to use the Main command, then you must set the "program" entry of your configuration to the latter. Here is an example function:
+
+program = function()
+  return vim.fn.expand('%p:')
+end
+
+This works the same as setting program = '${file}', except that this way you can use Main. When you call Main, dap-hot-config.nvim will execute your function and store the output as the value for "program" in your configuration. Now that the value for "program" is a string, and not a format string, the same program will be run for debugging every time. Specifically, the buffer you had open when you called Main.
+
+The default function is not forgotten, though. Therefore, you can still call Main on another buffer.
+
+### :Unmain
+
+Undoes the effect of Main. While Main turns your the value of "program" to a string, Unmain turns it back into the function you created.
+
+## Limitations
+
+Currently, dap-hot-config.nvim only expects one configuration per filetype. If you have two Python configurations, for example, then Argv/Main/Unmain will only modify the first. The workaround is to order your configurations so that the one you want to edit on-the-fly the most is first.
+
+Also, you can't use Main to run a program that uses a different filetype.
+
+Using commands to configure your debug launch is very cool, but this plugin only lets you modify the arguments given and the program to be launched. Perhaps this plugin could also modify the environment variables visible to the debuggee or other cool shit.
---
+++ lua/dap-hot-config.lua
@@ -0,0 +1,152 @@
+-- For internal use, get the configuration that should be edited based on that filetype. If DAP has multiple
+-- configurations associated with that filetype, just get the first one.
+-- If the configuration is missing, print a message and return nil.
+-- TODO: maybe this plugin could modify all configurations associated with the filetype, instead of just the first one.
+function GetConfig(filetype)
+  -- Get the appropriate table of DAP configurations using the filetype of the current buffer, and make sure it's valid
+  local config_table = require'dap'.configurations[filetype]
+  if config_table == nil then
+    print(filetype .. " not found in require'dap'.configurations")
+    return nil
+  end
+
+  local config = config_table[1]
+  if config == nil then
+    print("No configuration found in require'dap'.configurations." .. filetype)
+  end
+  return config
+end
+
+-- Similar to GetConfig, but also checks that the configuration has a _default_program field, which is automatically
+-- added to all (Un)Main-able configurations in Setup
+function GetConfigWithDefaultProgram(filetype)
+  local config = GetConfig(filetype)
+  if config == nil then return nil end
+
+  if config._default_program == nil then
+    print("require'dap'.configurations." .. filetype .. "[1].program is not set to a function by default")
+    print("Example function that returns the path to the file being currently edited:")
+    print("function()\n  return vim.fn.expand('%p:)\nend")
+    return nil
+  end
+
+  return config
+end
+
+-- Edit the `program` field of the appropriate dap configuration
+-- If the default value of this field was appropriately set to a function, then run the function and modify the field
+-- to hold a string, the return value.
+function Main()
+  local filetype = vim.bo.filetype
+  local config = GetConfigWithDefaultProgram(filetype)
+  if config == nil then return end
+
+  config.program = config._default_program()
+  print("require'dap'.configurations'." .. filetype .. "[1].program = " .. config.program)
+end
+
+-- Edit the `program` field of the appropriate dap configuration
+-- If the default value of this field was appropriately set to a function, then set the field back to its default value
+function Unmain()
+  local filetype = vim.bo.filetype
+  local config = GetConfigWithDefaultProgram(filetype)
+  if config == nil then return end
+
+  config.program = config._default_program
+  print("require'dap'.configurations'." .. filetype .. "[1].program reset")
+  return true
+end
+
+-- Undo the function Main
+function Unmain()
+  local filetype = vim.bo.filetype
+  local config = GetConfig(filetype)
+  if config == nil then return end
+
+  if config._default_program == nil then
+    print("require'dap'.configurations." .. filetype .. "[1].program is not a function")
+    print("Example function that returns the path to the file being currently edited:")
+    print("function()\n  return vim.fn.expand('%p:)\nend")
+    return
+  end
+
+  config.program = config._default_program
+  print("require'dap'.configurations'." .. filetype .. "[1].program = " .. config.program)
+end
+
+-- Takes in an argument table from the nvim_create_user_command API. Accordingly adjusts the args table of the DAP
+-- configuration corresponding to the filetype of the current buffer. When successful, prints out the args table.
+-- The Vim command can look like any of the following:
+-- Argv .................. only prints the args
+-- Argv pop .............. removes the last item in args
+-- Argv clear ............ sets args to an empty table
+-- Argv <index> .......... sets args[index] to an empty string
+-- Argv <index> <value> .. sets args[index] to value. value can start with whitespace, just put the whitespace after the
+--                           separating space between <index> and <value>
+function Argv(my)
+  local filetype = vim.bo.filetype
+  local config = GetConfig(filetype)
+  if config == nil then return end
+
+  -- If there is no args table, make an empty one before doing anything!
+  config.args = config.args or {}
+
+  -- Helper function to print the current args table
+  local function print_args()
+    print("require'dap'.configurations." .. filetype .. "[1].args")
+    for k, v in ipairs(config.args) do
+      print(tostring(k) .. "=" .. v) -- Prints "k=v"
+    end
+  end
+
+  -- If the user passed no arguments, simply print out all configured arguments
+  if my.args == '' then
+    print_args()
+    return
+  end
+
+  -- Check if the user wants the 'pop' or 'clear' subcommands
+  if my.args == 'pop' then
+    config.args[#config.args] = nil -- Remove the last item in the args table
+    print_args()
+    return
+  elseif my.args == 'clear' then
+    config.args = {} -- Remove every item in the args table
+    print_args()
+    return
+  end
+
+  -- No subcommand given, so the user must want to modify the args table at a specific index
+  -- Get the index of the argument that the user wants to modify, and make sure it is valid
+  local index = tonumber(my.fargs[1])
+  if index == nil or index % 1 ~= 0 then
+    print(my.fargs[1] .. " is not a valid index or 'pop' or 'clear'")
+    return
+  elseif index <= 0 or index > #config.args + 1 then
+    print(tostring(index) .. " is out of bounds for the current args table")
+    return
+  end
+
+  -- Get the value that the user wants to put at that index, which is an empty string if nothing is given
+  local value = my.args:sub(string.len(my.fargs[1]) + 2)
+
+  -- Apply the configuration and print out the args
+  config.args[index] = value
+  print_args()
+end
+
+function Setup()
+  -- Sneakily store the default value for `program` in each primary configuration as `_default_program`
+  -- By primary configuration, I mean the first configuration in the table associated with any filetype.
+  for _, config_table in require'dap'.configurations do
+    local config = config_table[1]
+    if config and type(config.program) == 'function' then
+      config._default_program = config.program
+    end
+  end
+
+  -- Set user commands
+  vim.api.nvim_create_user_command('Main', Main, {})
+  vim.api.nvim_create_user_command('Unmain', Unmain, {})
+  vim.api.nvim_create_user_command('Argv', Argv, { nargs = '*' })
+end
